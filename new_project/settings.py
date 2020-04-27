637d628d812f753b72793f3435d9865379c660b0
Ashishgirigoswami <43043428+Ashishgirigoswami@users.noreply.github.com>
2020-04-27 17:29:52 +0530

    Update settings.py

--- new_project/settings.py
+++ new_project/settings.py
@@ -138,8 +138,8 @@ CRISPY_TEMPLATE_PACK= 'bootstrap4'
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'giriashish11978197@gmail.com'
-EMAIL_HOST_PASSWORD = 't@ashish1799'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
